fe93d49326888bdea77cc01b5c2f13dbad0fe61a
ristofer <cristopher.gomez@ug.uchile.cl>
2021-12-31 20:51:35 -0300

    se completa clase Portal

--- representations/Portal.js
+++ representations/Portal.js
@@ -6,4 +6,52 @@ var Portal = function () {
     }
 };
 
+Portal.prototype.obtenerDmx = function obtenerDmx(id) {
+    return this.dmxs[id];
+}
+
+Portal.prototype.moverPitch = function moverPitch(angle) {
+    for (var i = 0; i < this.dmxs.length; i++) {
+        this.dmxs[i].moverPitch(angle);
+    }
+    return this;
+}
+
+Portal.prototype.moverYaw = function moverYaw(angle) {
+    for (var i = 0; i < this.dmxs.length; i++) {
+        this.dmxs[i].moverYaw(angle);
+    }
+    return this;
+}
+
+Portal.prototype.esperar = function esperar(tiempo){
+    for (var i = 0; i < this.dmxs.length; i++) {
+        this.dmxs[i].esperar(tiempo);
+    }
+    return this;
+}
+
+Portal.prototype.cambiarRapidezYaw = function cambiarRapidezYaw(vel){
+    for (var i = 0; i < this.dmxs.length; i++) {
+        this.dmxs[i].cambiarRapidezYaw(vel);
+    }
+    return this;
+}
+
+Portal.prototype.cambiarRapidezPitch = function cambiarRapidezPitch(vel){
+    for (var i = 0; i < this.dmxs.length; i++) {
+        this.dmxs[i].cambiarRapidezPitch(vel);
+    }
+    return this;
+}
+
+Portal.prototype.cambiarColorLuz = function cambiarColorLuz(color, intensity){
+    for (var i = 0; i < this.dmxs.length; i++) {
+        this.dmxs[i].cambiarColorLuz(color, intensity);
+    }
+    return this;
+}
+
+
+
 window["Portal"] = Portal; 
